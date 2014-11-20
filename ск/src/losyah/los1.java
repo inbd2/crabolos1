f030a18c9b92a9838cc532d1802e82e03233bfe2
User <User@10.11.184.8>
2014-11-20 15:29:39 +0400

    Commit 3

---
+++ ск/src/losyah/los1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package losyah;
+
+/**
+ *
+ * @author User
+ */
+public class los1 {
+    public static void main(String s[])
+    { 
+        System.out.println("I'm born!");
+    
+    }
+}
